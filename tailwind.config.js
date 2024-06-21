99ccf67146ca86b8a225638efe8d65c9594d3d2d
Temijopelo Jeje <temijeje@gmail.com>
2024-06-21 01:25:45 +0100

    finished task

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {},
+      fontFamily: {
+        libre: ['"Secular One", sans-serif', ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [],
